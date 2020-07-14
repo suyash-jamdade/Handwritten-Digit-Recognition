ebf49e0f63dd07b624372d8017cdf359e399f35c
SUYASH JAMDADE <60968407+suyash-jamdade@users.noreply.github.com>
2020-07-14 20:20:39 +0530

    Update handwritten_digit_recognition.py

--- handwritten_digit_recognition.py
+++ handwritten_digit_recognition.py
@@ -3,6 +3,14 @@ import numpy as np
 
 mnist = tf.keras.datasets.mnist
 
+class myCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if(logs.get('accuracy')>0.99):
+            print("\nReached 97.0% accuracy so cancelling training!")
+            self.model.stop_training = True
+
+callbacks = myCallback()
+
 (training_images, training_labels), (test_images, test_labels) = mnist.load_data()
 training_images = training_images.reshape(60000, 28, 28, 1) / 255.0
 
@@ -19,7 +27,7 @@ model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
 model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-history = model.fit(training_images , training_labels , epochs = 20 )
+history = model.fit(training_images , training_labels , epochs = 20 ,callbacks=[callbacks])
 print(history.epoch, history.history['accuracy'][-1])
 
 val_loss, val_acc = model.evaluate(test_images, test_labels)
@@ -30,7 +38,7 @@ print(len(prediction))
 
 print("The number on the picture is {} ".format(np.argmax(prediction[66])))
 
-
+model.save('mnist_handwritten.h5')
 
 
 
